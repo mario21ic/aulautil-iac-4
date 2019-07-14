91f5bc24cb47b21d52fa5862881634ca80e95c82
Marioi Inga <mario21ic@gmail.com>
2019-07-14 14:50:15 -0500

    Agregando module ec2

---
+++ main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = "${var.region}"
+}
+terraform {
+  backend "s3" {
+    bucket = "mario21ic.terraform.state"
+    key = "infrav1/terraform.tfstate"
+    region = "us-west-2"
+  }
+  required_version = ">0.9.4"
+}
+
+variable "region" {
+}
+
+output "ip" {
+  value = "${module.web.ip}"
+}
+
+output "sg_id" {
+  value = "${module.web.sg_id}"
+}
+
+module "web" {
+  source = "./tfmodules/ec2/"
+
+  region = "${var.region}"
+  env = "${terraform.workspace}"
+  ami_name = "mynginx"
+  key_name = "demokp"
+  nombre = "demo"
+}
+
+resource "aws_elb" "demo" {
+ name               = "${terraform.workspace}-demo"
+ availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
+
+ listener {
+   instance_port     = 80
+   instance_protocol = "http"
+   lb_port           = 80
+   lb_protocol       = "http"
+ }
+
+ health_check {
+   healthy_threshold   = 10
+   unhealthy_threshold = 6
+   timeout             = 10
+   target              = "HTTP:80/"
+   interval            = 30
+ }
+
+ instances                   = ["${module.web.ids}"]
+ cross_zone_load_balancing   = true
+ idle_timeout                = 400
+ connection_draining         = true
+ connection_draining_timeout = 400
+}
+
